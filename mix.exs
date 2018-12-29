834ade7c9cdb14f9fe21a998b532bb27b5ee7d75
Paul Williams <paul@pdwilliams.net>
2018-12-29 19:15:27 +1100

    Clean up MixProject

--- mix.exs
+++ mix.exs
@@ -6,23 +6,13 @@ defmodule ElixirAsciiImage.MixProject do
       app: :elixir_ascii_image,
       version: "0.1.0",
       elixir: "~> 1.7",
-      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      extra_applications: [:logger]
-    ]
-  end
-
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
-      {:mogrify, "~> 0.7.0"}
     ]
   end
 end
